76877ef5b4ce5f0f03b82a13fb4f9b3ff2c0f1d5
Sh0lf <vyvincentyap@gmail.com>
2023-04-07 12:28:25 +0200

    testing fxml scenes

--- src/main/java/com/isep/hpah/views/scenes/applications/IntroductionApplication.java
+++ src/main/java/com/isep/hpah/views/scenes/applications/IntroductionApplication.java
@@ -1,4 +1,23 @@
 package com.isep.hpah.views.scenes.applications;
 
-public class IntroductionApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class                                                                                                                                                                                                   IntroductionApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(IntroductionApplication.class.getResource("introduction.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
--- src/main/java/com/isep/hpah/views/scenes/controller/IntroductionController.java
+++ src/main/java/com/isep/hpah/views/scenes/controller/IntroductionController.java
@@ -1,4 +1,14 @@
 package com.isep.hpah.views.scenes.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
 public class IntroductionController {
+        @FXML
+        private Label label;
+
+        @FXML
+        protected void onButtonClick() {label.setText("Get ready !!!");
+        }
 }
